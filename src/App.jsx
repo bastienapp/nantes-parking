b34a5aa8c06b03403a57ed0fffbc9f3555e2eb38
bastienwcs <bastien.krettly@wildcodeschool.com>
2020-11-02 17:04:08 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ParkingList from './components/ParkingList';
+
+function App() {
+  return (
+    <div className="App">
+      <ParkingList />
+    </div>
+  );
+}
+
+export default App;
